477d43dc8a234611f00629674cf3885f302db52f
AR-RIYAD <134699041+AR-RIYAD@users.noreply.github.com>
2024-03-07 15:13:33 +0600

    First structure of my portfolio with react

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,36 @@
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="header-left-side">
+          <div className="content">
+            <div className="img">
+                
+            </div>
+            <h1>
+              Abdur Rahman
+            </h1>
+          </div>
+          <h2>-Web Developer</h2>
+          <p>
+            Hey! I am a proficient student currently pursuing my fifth semester in Computer Science and Engineering at Patuakhali Science and Technology University. With expertise in languages such as C, C++, Java, along with web development skills in HTML, CSS, and JavaScript, I also possess proficiency in React, showcasing a versatile skill set poised for software development roles.
+          </p>
+          <button>HTML</button>
+          <button>CSS</button>
+          <br />
+          <br />
+          <button>JavaScript</button>
+          <button>React</button> <br />
+          <br />
+          <button>C</button>
+          <button>C++</button> <br />
+          <br />
+          <button className="btn">Hire now</button>
+          <h3>Copyright © 2024 Abdur Rahman - Web Developer</h3>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar(){
+
+    return (
+        <>
+        <div className="navbar">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Resume</a></li>
+                <li><a href="#">Hire Me</a></li>
+            </ul>
+            </div>
+        </>
+    )
+}
+
+
+export default Navbar;
